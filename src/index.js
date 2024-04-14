86ccebe7e40c38c3d2753558a6913bcc7a67d1cb
A.Westwood <personal.email.aw1@gmail.com>
2024-04-14 13:49:14 +0100

    05. js for add todos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+import { add } from 'lodash';
 import './styles.css';
 
 
 console.log('hello world');
 
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
+const addToDo = document.getElementById("addToDo").addEventListener("click", addTask);
 
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        }
+
+    inputBox.value = "";
+}
