8ab961bbe5b138de394026e51ef66a568d6fce1b
A.Westwood <personal.email.aw1@gmail.com>
2024-04-25 14:14:39 +0100

    10. create js for delete list btn

--- src/index.js
+++ src/index.js
@@ -8,12 +8,26 @@ console.log('hello world');
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListBtn = document.querySelector('[data-delete-list-btn]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.lists'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-let selectedListId = localStorage.getItem()
+let selectedListId = localStorage.getItem
 (LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+
+deleteListBtn.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
 
 newListForm.addEventListener('submit', e=> {
     e.preventDefault()
@@ -35,7 +49,8 @@ function saveAndRender() {
 }
 
 function save() {
-    localStorage.setItem(JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 function render(){
@@ -61,34 +76,34 @@ function clearElement(element) {
 render()
 
 
-//to do list js
+// //to do list js
 
-const inputBox = document.getElementById("inputBox");
-const listContainer = document.getElementById("listContainer");
-const addToDo = document.getElementById("addToDo").addEventListener("click", addTask);
+// const inputBox = document.getElementById("inputBox");
+// const listContainer = document.getElementById("listContainer");
+// const addToDo = document.getElementById("addToDo").addEventListener("click", addTask);
 
 
-function addTask(){
-    if(inputBox.value === ""){
-        alert("You must write something");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-        }
+// function addTask(){
+//     if(inputBox.value === ""){
+//         alert("You must write something");
+//     }
+//     else{
+//         let li = document.createElement("li");
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+//         let span = document.createElement("span");
+//         span.innerHTML = "\u00d7";
+//         li.appendChild(span);
+//         }
 
-    inputBox.value = "";
-};
+//     inputBox.value = "";
+// };
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){ 
-        e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-    }
-}, false);
+// listContainer.addEventListener("click", function(e){
+//     if(e.target.tagName === "LI"){ 
+//         e.target.classList.toggle("checked");
+//     }
+//     else if(e.target.tagName === "SPAN"){
+//         e.target.parentElement.remove();
+//     }
+// }, false);
