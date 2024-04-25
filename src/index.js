dab95295e94fc91acd27de7b363378cb4ed638e7
A.Westwood <personal.email.aw1@gmail.com>
2024-04-25 14:35:41 +0100

    11. move remaining indicator and add list title and add appropriate css

--- src/index.js
+++ src/index.js
@@ -78,32 +78,32 @@ render()
 
 // //to do list js
 
-// const inputBox = document.getElementById("inputBox");
-// const listContainer = document.getElementById("listContainer");
-// const addToDo = document.getElementById("addToDo").addEventListener("click", addTask);
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
+const addToDo = document.getElementById("addToDo").addEventListener("click", addTask);
 
 
-// function addTask(){
-//     if(inputBox.value === ""){
-//         alert("You must write something");
-//     }
-//     else{
-//         let li = document.createElement("li");
-//         li.innerHTML = inputBox.value;
-//         listContainer.appendChild(li);
-//         let span = document.createElement("span");
-//         span.innerHTML = "\u00d7";
-//         li.appendChild(span);
-//         }
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        }
 
-//     inputBox.value = "";
-// };
+    inputBox.value = "";
+};
 
-// listContainer.addEventListener("click", function(e){
-//     if(e.target.tagName === "LI"){ 
-//         e.target.classList.toggle("checked");
-//     }
-//     else if(e.target.tagName === "SPAN"){
-//         e.target.parentElement.remove();
-//     }
-// }, false);
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){ 
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
