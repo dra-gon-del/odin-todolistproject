90c3783673854482ab4a2a63c1085b505911604f
A.Westwood <personal.email.aw1@gmail.com>
2024-05-08 13:36:03 +0100

    22. write js for adding new project input to project list

--- src/index.js
+++ src/index.js
@@ -6,21 +6,40 @@ console.log('hello world');
 //project list js
 
 const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = ['name', 'todo'];
+let lists = [{id: 1, name: 'name'}, {id: 2, name: 'todo'}];
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName === '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render();
+});
+
+function createList(name) {
+    return {id: Date.now().toString(), name: name, tasks: []}
+};
 
 function render() {
     clearElement(listsContainer);
     lists.forEach(list => {
         const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
         listElement.classList.add("projectListName");
-        listElement.innerText = list;
+        listElement.innerText = list.name;
         listsContainer.appendChild(listElement);
     });
 }
 
 function clearElement(element){
-
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    };
 };
 
 render()
