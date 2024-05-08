5a5554908c9a0512ba2c61052a039a73de45dcdc
A.Westwood <personal.email.aw1@gmail.com>
2024-05-08 12:45:31 +0100

    21. create js for project list

--- src/index.js
+++ src/index.js
@@ -5,3 +5,22 @@ console.log('hello world');
 
 //project list js
 
+const listsContainer = document.querySelector('[data-lists]');
+
+let lists = ['name', 'todo'];
+
+function render() {
+    clearElement(listsContainer);
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.classList.add("projectListName");
+        listElement.innerText = list;
+        listsContainer.appendChild(listElement);
+    });
+}
+
+function clearElement(element){
+
+};
+
+render()
