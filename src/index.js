915069c7e060650380dc8acd3fe44c8fb5b2b5b0
A.Westwood <personal.email.aw1@gmail.com>
2024-05-08 16:21:58 +0100

    23. write js for save and render project list items

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = [{id: 1, name: 'name'}, {id: 2, name: 'todo'}];
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -18,11 +19,20 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
-    render();
+    saveAndRender();
 });
 
 function createList(name) {
-    return {id: Date.now().toString(), name: name, tasks: []}
+    return {id: Date.now().toString(), name: name, tasks: []};
+};
+
+function saveAndRender() {
+    save();
+    render();
+};
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
 };
 
 function render() {
@@ -34,7 +44,7 @@ function render() {
         listElement.innerText = list.name;
         listsContainer.appendChild(listElement);
     });
-}
+};
 
 function clearElement(element){
     while (element.firstChild) {
@@ -42,4 +52,4 @@ function clearElement(element){
     };
 };
 
-render()
+render();
