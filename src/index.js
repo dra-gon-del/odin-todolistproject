bba60d1e3a0e8712e7741e6f89c6bb9ffcaaeb62
A.Westwood <personal.email.aw1@gmail.com>
2024-05-09 13:00:10 +0100

    25. add js for delete list btn

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ console.log('hello world');
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-btn]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -20,6 +21,12 @@ listsContainer.addEventListener('click', e => {
         selectedListId = e.target.dataset.listId;
         saveAndRender();
     };
+});
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId);
+    selectedListId = null;
+    saveAndRender();
 });
 
 newListForm.addEventListener('submit', e => {
